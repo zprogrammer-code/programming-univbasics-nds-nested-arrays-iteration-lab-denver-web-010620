3a32c6f30f1ceed0eaa2302a9fd9f9ef925616d4
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-28 15:34:13 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,4 +1,11 @@
 def join_ingredients(src)
+  ingredients = [
+    ["pepperoni","sausage",],
+    ["green olives","green peppers"],
+    ["onions","pineapple"]]
+    array = ["I love #{ingredients[0][0]} and #{ingredients[0][1]} on my pizza", "I love #{ingredients[1][0]} and #{ingredients[1][1]} on my pizza", "I love #{ingredients[2][0]} and #{ingredients[2][1]} on my pizza"]
+    
+    return array
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
   #
   # Build a new Array that contains strings where each pair of foods is
@@ -9,12 +16,45 @@ def join_ingredients(src)
 end
 
 def find_greater_pair(src)
+    greater_number = []
+    row_index = 0
+while row_index < src.count do
+  element_index = 0
+  
+    if src[row_index][0] > src[row_index][1]
+      greater_number << src[row_index][0]
+    else
+      greater_number << src[row_index][1]
+end
+   element_index += 1
+  row_index += 1 
+end
+return greater_number
+end
+
+
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
-end
 
 def total_even_pairs(src)
+    total = 0
+    row_index = 0
+    even_pair = []
+    while row_index < src.count do 
+   
+   if src[row_index][0]%2 == 0 && src[row_index][1]%2 == 0
+     even_pair << src[row_index][0]
+     even_pair << src[row_index][1]
+        
+         total += even_pair[0]
+         total += even_pair[1]
+        #binding.pry
+
+      end
+    row_index += 1
+    end
+  return total
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
   # total
@@ -23,3 +63,5 @@ def total_even_pairs(src)
   # the number was even. Review the operator documentation if you've forgotten
   # this!
 end
+
+
